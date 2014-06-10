097cfe57ae70f09094fd45bb907c6f8b46ebbad2
Ethan Nagel <eanagel@gmail.com>
2014-06-10 07:55:05 -0700

    Initial Commit

---
+++ NTSwiftJson/NTSwiftJson.h
@@ -0,0 +1,19 @@
+//
+//  NTSwiftJson.h
+//  NTSwiftJson
+//
+//  Created by Ethan Nagel on 6/10/14.
+//  Copyright (c) 2014 NagelTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NTSwiftJson.
+FOUNDATION_EXPORT double NTSwiftJsonVersionNumber;
+
+//! Project version string for NTSwiftJson.
+FOUNDATION_EXPORT const unsigned char NTSwiftJsonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NTSwiftJson/PublicHeader.h>
+
+
